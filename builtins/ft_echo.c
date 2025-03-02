1360977cd317b8d9420fb1df3eabe88f53682451
Ahmed Anmazir <aanmazir@c3r10p3.1337.ma>
2025-03-02 15:36:40 +0000

    echo 0 any path is working

--- builtins/ft_echo.c
+++ builtins/ft_echo.c
@@ -1,5 +1,18 @@
 #include "../minishell.h"
 
+void echo_path(char *path)
+{
+    int j;
+
+    j = 0;
+    while (environ[j] != NULL)
+    {
+        if (strncmp(environ[j], path, ft_strlen(path)) == 0)
+            printf("%s", environ[j] + ft_strlen(path) + 1);
+        j++;
+    }
+}
+
 int ft_echo(t_command *cmd)
 {
     int i;
@@ -12,21 +25,26 @@ int ft_echo(t_command *cmd)
         no_newline = 1;
         i++;
     }
-    while (i < cmd->nb_arg)
+    if (cmd->args && strncmp(cmd->args[1], "$", 1) == 0)
+        echo_path(cmd->args[1] + 1);
+    else
     {
-        char *arg = cmd->args[i];
-        if (arg[0] == '"' && arg[strlen(arg) - 1] == '"')
+        while (i < cmd->nb_arg)
         {
-            arg[strlen(arg) - 1] = '\0';
-            printf("%s", arg + 1);
+            char *arg = cmd->args[i];
+            if (arg[0] == '"' && arg[strlen(arg) - 1] == '"')
+            {
+                arg[strlen(arg) - 1] = '\0';
+                printf("%s", arg + 1);
+            }
+            else
+                printf("%s", cmd->args[i]);
+            if (i < cmd->nb_arg - 1)
+            {
+                printf(" ");
+            }
+            i++;
         }
-        else
-            printf("%s", cmd->args[i]);
-        if (i < cmd->nb_arg - 1)
-        {
-            printf(" ");
-        }
-        i++;
     }
     if (!no_newline)
         printf("\n");
