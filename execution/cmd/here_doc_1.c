88c73e162443893dd21667594a53a9ccaa72a9c9
Ahmed Anmazir <aanmazir@c3r2p9.1337.ma>
2025-04-25 16:43:26 +0100

    mkstemp fixed 60%

--- execution/cmd/here_doc_1.c
+++ execution/cmd/here_doc_1.c
@@ -6,11 +6,50 @@
 /*   By: aanmazir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 11:35:00 by aanmazir          #+#    #+#             */
-/*   Updated: 2025/04/24 11:39:53 by aanmazir         ###   ########.fr       */
+/*   Updated: 2025/04/25 16:42:08 by aanmazir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
+
+void	generate_random_chars(char *str, size_t len)
+{
+	size_t				i;
+	static const char	*chars;
+
+	i = 0;
+	chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	while (i < len)
+	{
+		str[i] = chars[rand() % (sizeof(chars) - 1)];
+		i++;
+	}
+	str[len] = '\0';
+}
+
+int	ft_mkstemp(char *template)
+{
+	char	*xx_pos;
+	int		attempt;
+	int		fd;
+
+	if (template == NULL || ft_strlen(template) < 6)
+		return (-1);
+	xx_pos = strstr(template, "XXXXXX");
+	if (xx_pos == NULL)
+		return (-1);
+	srand((unsigned int)time(NULL));
+	attempt = 0;
+	while (attempt < 10000)
+	{
+		generate_random_chars(xx_pos, 6);
+		fd = open(template, O_RDWR | O_CREAT | O_EXCL, 0600);
+		if (fd != -1)
+			return (fd);
+		attempt++;
+	}
+	return (-1);
+}
 
 void	init_heredoc_signals(struct sigaction *old_sa)
 {
@@ -34,52 +73,11 @@ int	create_heredoc_file(char *template)
 {
 	int	fd;
 
-	fd = mkstemp(template);
+	fd = ft_mkstemp(template);
 	if (fd < 0)
 	{
 		ft_putstr_fd("mkstemp error\n", 2);
 		return (-1);
 	}
 	return (fd);
-}
-
-int	read_heredoc_and_write(t_heredoc_ctx *ctx)
-{
-	struct sigaction	old_sa;
-	char				*line;
-	int					status;
-
-	init_heredoc_signals(&old_sa);
-	while (1)
-	{
-		line = readline("> ");
-		if (!line || g_last_signal == SIGINT)
-			break ;
-		status = handle_heredoc_line(ctx, line);
-		free(line);
-		if (status != 0)
-			break ;
-	}
-	restore_heredoc_signals(&old_sa);
-	return (g_last_signal == SIGINT);
-}
-
-char	*handle_heredoc(const char *delimiter, int is_quoted, t_shell *shell)
-{
-	t_heredoc_ctx	ctx;
-	char			*template;
-	int				interrupted;
-	int				fd;
-
-	if (open_heredoc_tmp(&template, &fd) < 0)
-		return (NULL);
-	ctx.fd = fd;
-	ctx.is_quoted = is_quoted;
-	ctx.delimiter = delimiter;
-	ctx.shell = shell;
-	interrupted = read_heredoc_and_write(&ctx);
-	if (interrupted)
-		return (heredoc_on_interrupt(shell, template, fd));
-	close(fd);
-	return (template);
 }
--- execution/cmd/here_doc_line.c
+++ execution/cmd/here_doc_line.c
@@ -12,6 +12,47 @@
 
 #include "../../minishell.h"
 
+int	read_heredoc_and_write(t_heredoc_ctx *ctx)
+{
+	struct sigaction	old_sa;
+	char				*line;
+	int					status;
+
+	init_heredoc_signals(&old_sa);
+	while (1)
+	{
+		line = readline("> ");
+		if (!line || g_last_signal == SIGINT)
+			break ;
+		status = handle_heredoc_line(ctx, line);
+		free(line);
+		if (status != 0)
+			break ;
+	}
+	restore_heredoc_signals(&old_sa);
+	return (g_last_signal == SIGINT);
+}
+
+char	*handle_heredoc(const char *delimiter, int is_quoted, t_shell *shell)
+{
+	t_heredoc_ctx	ctx;
+	char			*template;
+	int				interrupted;
+	int				fd;
+
+	if (open_heredoc_tmp(&template, &fd) < 0)
+		return (NULL);
+	ctx.fd = fd;
+	ctx.is_quoted = is_quoted;
+	ctx.delimiter = delimiter;
+	ctx.shell = shell;
+	interrupted = read_heredoc_and_write(&ctx);
+	if (interrupted)
+		return (heredoc_on_interrupt(shell, template, fd));
+	close(fd);
+	return (template);
+}
+
 char	*expand_heredoc_line(const char *line, t_shell *shell)
 {
 	char	*result;
